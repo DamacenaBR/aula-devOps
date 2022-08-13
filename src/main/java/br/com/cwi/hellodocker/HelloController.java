4e0b558fe917ce0c09ca56c01756d41c76488d15
Bruno Silva Damacena <bruno.damacena@cwi.com.br>
2022-08-13 10:36:00 -0300

    atualizando pipeline 2

--- src/main/java/br/com/cwi/hellodocker/HelloController.java
+++ src/main/java/br/com/cwi/hellodocker/HelloController.java
@@ -4,10 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Request@GetMapping("/aula")
 public class HelloController {    
-
-    @GetMapping("/bucarIndex")
+    @GetMapping("/")
     public String index() {
         return "Index!";
     }
